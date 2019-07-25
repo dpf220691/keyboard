c14053c0c8429ac6bb8e52e4d33ef51bcf876bbd
David Fernandez <david.fernandez.jimenez@ing.com>
2019-07-25 08:36:00 +0200

    Add problem code

---
+++ src/main/java/com/ing/mwchapter/App.java
@@ -0,0 +1,8 @@
+package com.ing.mwchapter;
+
+
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "\"Talent wins games, but teamwork and intelligence win championships.\" – Michael Jordan" );
+    }
+}
---
+++ src/main/java/com/ing/mwchapter/services/impl/KeyboardServiceImpl.java
@@ -0,0 +1,16 @@
+package com.ing.mwchapter.services.impl;
+
+import com.ing.mwchapter.services.IKeyboardService;
+import javax.validation.constraints.NotNull;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class KeyboardServiceImpl implements IKeyboardService {
+
+    @Override
+    public int entryTime(@NotNull String code, @NotNull String keypad) {
+        //TODO: Implement me
+        throw new NotImplementedException();
+
+    }
+
+}
